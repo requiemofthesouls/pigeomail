bbb577a075c8db7317614728701b9a9f16d21cce
Konstantin Kiselev <dimamid99@gmail.com>
2022-01-28 17:28:17 +0300

    feature/ci-cd restructure deploy

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -2,6 +2,7 @@ PIGEOMAIL_VERSION="$1"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "PIGEOMAIL_VERSION=$PIGEOMAIL_VERSION" > "$SCRIPT_DIR"/.env
-docker-compose up -f "$SCRIPT_DIR"/.deploy/docker-compose.yml -d
+docker-compose -f "$SCRIPT_DIR"/.deploy/docker-compose.yml -f "$SCRIPT_DIR"/.deploy/docker-compose.prod.yml pull
+docker-compose -f "$SCRIPT_DIR"/.deploy/docker-compose.yml -f "$SCRIPT_DIR"/.deploy/docker-compose.prod.yml up -d
 
 exit 0
