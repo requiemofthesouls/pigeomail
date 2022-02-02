49ad386441c094d42a4e98c50d8ffd54a4edb2b0
Nikita Kuzmin <kobalt31@mail.ru>
2022-02-02 11:10:35 +0300

    Add mutex to users map

--- internal/fsm/users_manager.go
+++ internal/fsm/users_manager.go
@@ -1,5 +1,7 @@
 package fsm
 
+import "sync"
+
 // User states
 const (
 	Idle State = iota
@@ -36,9 +38,10 @@ var (
 
 // UsersManager keeps states of all users and manages it
 type UsersManager struct {
-	idleState   State // init state for every new user
-	Fsm         FSM
-	usersStates map[int64]State
+	idleState     State // init state for every new user
+	Fsm           FSM
+	usersStates   map[int64]State
+	usersStatesMu sync.RWMutex
 }
 
 func NewUserManager() *UsersManager {
@@ -51,6 +54,9 @@ func NewUserManager() *UsersManager {
 
 // GetState get state for user. If no user found - manager saves user and return idle state
 func (um *UsersManager) GetState(userId int64) (state State) {
+	um.usersStatesMu.RLock()
+	defer um.usersStatesMu.RUnlock()
+
 	if state, ok := um.usersStates[userId]; ok {
 		return state
 	}
@@ -61,6 +67,10 @@ func (um *UsersManager) GetState(userId int64) (state State) {
 // SendEventE send event for user. If no user found - user is considered to be in idle state
 func (um *UsersManager) SendEventE(userId int64, event Event) (newState State, err error) {
 	var currentState = um.GetState(userId)
+
+	um.usersStatesMu.RLock()
+	defer um.usersStatesMu.RUnlock()
+
 	if newState, err = um.Fsm.SendEvent(currentState, event); err == nil {
 		um.usersStates[userId] = newState
 	}
